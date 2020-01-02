eb2f4b4ba80b31ce4af9ba9e7715615311a7d182
biropost <billie.postlmayr@gmail.com>
2020-01-02 20:11:29 +0100

    data is all in export file can can be used by classification

--- DataSectioner.py
+++ DataSectioner.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 data_dir = "data"
 all_trips = listdir(data_dir)
-#whitelist_ids = [5, 46, 74, 127, 128, 129, 131, 165, 99, 105, 107, 108, 109, 152, 202, 203, 13, 28, 43, 52, 145, 146, 22, 24, 26, 42, 102, 103, 147, 214, 215, 19, 134, 137, 139, 142, 220, 222, 223, 224, 26, 248, 246, 245, 241, 84, 93, 235, 236, 33, 36, 37, 40, 49, 120, 113, 114, 115, 116, 117, 218, 219, 226, 227, 240, 21, 32, 38, 70, 95, 199, 78, 82, 83, 160, 161, 208, 209, 55, 9, 35, 88, 210, 211, 217, 72, 47, 201, 204, 91]
-whitelist_ids = [99]
+whitelist_ids = [5, 46, 74, 127, 128, 129, 131, 165, 99, 105, 107, 108, 109, 152, 202, 203, 13, 28, 43, 52, 145, 146, 22, 24, 26, 42, 102, 103, 147, 214, 215, 19, 134, 137, 139, 142, 220, 222, 223, 224, 26, 248, 246, 245, 241, 84, 93, 235, 236, 33, 36, 37, 40, 49, 120, 113, 114, 115, 116, 117, 218, 219, 226, 227, 240, 21, 32, 38, 70, 95, 199, 78, 82, 83, 160, 161, 208, 209, 55, 9, 35, 88, 210, 211, 217, 72, 47, 201, 204, 91]
+#whitelist_ids = [99]
 
 def filter_whitelisted_trips(trip):
     # skip files that are not a trip
@@ -39,10 +39,15 @@ def formatTime(m):
 
 df = None
 
+processed = 0
+total = len(my_list)
+
 for trip in my_list:
     user_id = re.search('\d+', trip).group(0)
     trip_id = re.search('_\d+_', trip).group(0)
-    print("processing user: ",user_id, ", trip: ", trip_id)
+    trip_id = re.search('\d+', trip_id).group(0)
+    print("processing user: ",user_id, ", trip: ", trip_id, " --- (", processed, "/", total, ")")
+    processed = processed+1
     path_markers = path.join(data_dir, trip, 'markers.csv')
     col_names = ["value", "key", "time", "mode", "col5", "col6", "col7", "station", "col9"]
     markers = pd.read_csv(path_markers, sep=';', names=col_names, skiprows=4)
@@ -73,20 +78,14 @@ for trip in my_list:
     df_user = acceleration.merge(markers, on="time", how='left')
     df_user = df_user.ffill()
 
-    # drop n remaining rows for the 10 value segments
-    n, m = df_user.shape
-    rest = n % 10
-    df_user.drop(df_user.tail(rest).index, inplace=True)
-    df_user = df_user.iloc[30:]
-    df_user.drop(df_user.tail(30).index, inplace=True)
-
     # eliminate bimodal segments
+    df_user = df_user.reset_index()
     drop_multimodal = []
     for i in range(int(df_user.shape[0]/10)):
         counts = df_user.iloc[i*10:i*10+10].groupby('mode').count()
         if counts.shape[0] != 1:
             for val in range(i*10, i*10+10):
-                drop_multimodal.append(df_user.index[val])
+                drop_multimodal.append(val)
     df_user.drop(drop_multimodal, inplace=True)
 
     # add ids and add to overall dataframe

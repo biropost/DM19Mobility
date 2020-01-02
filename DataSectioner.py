add6959c0b49322d8c65463a7696f178751d0b75
biropost <billie.postlmayr@gmail.com>
2020-01-02 19:49:28 +0100

    preprocessing done

--- DataSectioner.py
+++ DataSectioner.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 
 data_dir = "data"
 all_trips = listdir(data_dir)
-whitelist_ids = [5, 46, 74, 127, 128, 129, 131, 165, 99, 105, 107, 108, 109, 152, 202, 203, 13, 28, 43, 52, 145, 146, 22, 24, 26, 42, 102, 103, 147, 214, 215, 19, 134, 137, 139, 142, 220, 222, 223, 224, 26, 248, 246, 245, 241, 84, 93, 235, 236, 33, 36, 37, 40, 49, 120, 113, 114, 115, 116, 117, 218, 219, 226, 227, 240, 21, 32, 38, 70, 95, 199, 78, 82, 83, 160, 161, 208, 209, 55, 9, 35, 88, 210, 211, 217, 72, 47, 201, 204, 91]
+#whitelist_ids = [5, 46, 74, 127, 128, 129, 131, 165, 99, 105, 107, 108, 109, 152, 202, 203, 13, 28, 43, 52, 145, 146, 22, 24, 26, 42, 102, 103, 147, 214, 215, 19, 134, 137, 139, 142, 220, 222, 223, 224, 26, 248, 246, 245, 241, 84, 93, 235, 236, 33, 36, 37, 40, 49, 120, 113, 114, 115, 116, 117, 218, 219, 226, 227, 240, 21, 32, 38, 70, 95, 199, 78, 82, 83, 160, 161, 208, 209, 55, 9, 35, 88, 210, 211, 217, 72, 47, 201, 204, 91]
+whitelist_ids = [99]
 
 def filter_whitelisted_trips(trip):
     # skip files that are not a trip
@@ -41,7 +42,7 @@ df = None
 for trip in my_list:
     user_id = re.search('\d+', trip).group(0)
     trip_id = re.search('_\d+_', trip).group(0)
-    print(user_id, trip_id)
+    print("processing user: ",user_id, ", trip: ", trip_id)
     path_markers = path.join(data_dir, trip, 'markers.csv')
     col_names = ["value", "key", "time", "mode", "col5", "col6", "col7", "station", "col9"]
     markers = pd.read_csv(path_markers, sep=';', names=col_names, skiprows=4)
@@ -50,6 +51,8 @@ for trip in my_list:
 
     path_acc = path.join(data_dir, trip, 'acceleration.csv')
     acceleration = pd.read_csv(path_acc, sep=',')
+    acceleration['acc_norm'] = np.linalg.norm(acceleration[['x', 'y', 'z']].values, axis=1)
+    acceleration = acceleration.drop(['x', 'y', 'z'], axis=1)
 
     path_activity = path.join(data_dir, trip, 'activity_records.csv')
     activity = pd.read_csv(path_activity, sep=',')
@@ -59,22 +62,40 @@ for trip in my_list:
     markers['time'] = formatTime(markers['time'])
     markers['time'] = markers['time'].astype(int)
 
+    # downsample
+    acceleration = acceleration.set_index(['time'])
+    acceleration.index = pd.to_datetime(acceleration.index, unit='ms')
+    acceleration = acceleration.resample('1L').mean()
+    markers = markers.set_index(['time'])
+    markers.index = pd.to_datetime(markers.index, unit='ms')
 
     # combine acc and markers
-    df_user = acceleration.merge(markers, on='time', how='inner')
+    df_user = acceleration.merge(markers, on="time", how='left')
     df_user = df_user.ffill()
 
     # drop n remaining rows for the 10 value segments
     n, m = df_user.shape
     rest = n % 10
     df_user.drop(df_user.tail(rest).index, inplace=True)
+    df_user = df_user.iloc[30:]
+    df_user.drop(df_user.tail(30).index, inplace=True)
+
+    # eliminate bimodal segments
+    drop_multimodal = []
+    for i in range(int(df_user.shape[0]/10)):
+        counts = df_user.iloc[i*10:i*10+10].groupby('mode').count()
+        if counts.shape[0] != 1:
+            for val in range(i*10, i*10+10):
+                drop_multimodal.append(df_user.index[val])
+    df_user.drop(drop_multimodal, inplace=True)
+
+    # add ids and add to overall dataframe
     df_user['user_id'] = user_id
     df_user['trip_id'] = trip_id
-
     if df is None:
         df = df_user.copy()
     else:
         df = df.append(df_user, ignore_index=True, sort=False)
 
-print(df.head())
-export_csv = df.to_csv('export_mobility.csv', index=None, header=True)
+print(df)
+export_csv = df.to_csv('export.csv', header=True)

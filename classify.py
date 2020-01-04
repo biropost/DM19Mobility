959ab843d54b8d0dc723aea68a61e36026339c41
Bernhard Frick <bernhard.frick@gmx.at>
2020-01-04 17:39:41 +0100

    rename classify to classify.py

--- classify.py
+++ classify.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from keras.models import Sequential
 from keras.layers import Dense
